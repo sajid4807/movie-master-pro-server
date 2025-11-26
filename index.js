54681beb007432a5212c599b2787d0f777631381
sajid4807 <mdsajidhossen4807@gmail.com>
2025-11-26 17:41:47 +0600

    added implement post api

--- index.js
+++ index.js
@@ -4,16 +4,57 @@ const app = express()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.port || 3000;
 
-
-const uri = `mongodb+srv://${process.env.MOVIE_USER}:${process.env.MOVIE_PASS}@cluster0.a47ogqg.mongodb.net/?appName=Cluster0`;
-
 // middleware
 app.use(cors())
 app.use(express.json())
 
+const uri = `mongodb+srv://${process.env.MOVIE_USER}:${process.env.MOVIE_PASS}@cluster0.a47ogqg.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+
 app.get('/',(req,res) => {
     res.send('movie master server is running pro')
 })
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db('movie_db')
+    const moviesCollection = db.collection("movies")
+    const userCollection = db.collection('user')
+    app.post('/user', async(req,res) => {
+      const newUser = req.body
+      const email = req.body.email
+      const query= {email: email}
+      const existingUser =await userCollection.findOne(query)
+      if(existingUser){
+        res.send({ message: 'user already exits. do not need to insert again' })
+      }
+      else{
+        const result = await userCollection.insertOne(newUser)
+        res.send(result)
+      }
+    })
+    
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } 
+  finally {
+
+  }
+}
+run().catch(console.dir);
+
 
 app.listen(port, () => {
     console.log(`movie master server is running port: ${port}`)
